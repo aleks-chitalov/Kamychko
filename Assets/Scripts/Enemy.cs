02c2306868804020640d984eecea6771ff739c41
alex-chitalov <alex55bg@gmail.com>
2021-09-13 23:49:24 +0300

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -27,7 +27,7 @@ public class Enemy : MonoBehaviour
         enemyHealth -= damage;
         if (enemyHealth <= 0)
         {
-            gameObject.SetActive(false);
+            gameObject.SetActive(true);
         }
     }
     public void EnemyStagger(float staggerTime)
